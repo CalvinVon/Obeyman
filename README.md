8dd0c3bf472f8b6c799903f0ecde8665e4517db9
Calvin <vip935721016@163.com>
2019-03-04 00:00:51 +0800

    update add doc; fix main entry of package

--- README.md
+++ README.md
@@ -1,2 +1,75 @@
 # Obeyman
-A simple object schema validate tool
+
+A simple object schema validate tool.
+(Simplified version of [hapijs/joi](https://github.com/hapijs/joi))
+
+[![version](https://img.shields.io/npm/v/obeyman.svg)](https://github.com/CalvinVon/Obeyman)
+![dependencies](https://img.shields.io/david/CalvinVon/obeyman.svg)
+[![](https://img.shields.io/npm/dt/obeyman.svg)](https://github.com/CalvinVon/Obeyman)
+[![](https://img.shields.io/github/size/CalvinVon/Obeyman/dist/obeyman.min.js.svg?label=minified%20size)](https://github.com/CalvinVon/Obeyman/blob/master/dist/obeyman.min.js)
+
+
+
+# Getting Started
+
+### Install
+
+via npm
+
+```bash
+npm i obeyman -S
+```
+
+# Usage
+
+```js
+// define schema
+const schema = Obeyman.object().keys({
+    userId: Obeyman.number().required(),
+    pageNum: Obeyman.number().required(),
+    pageSize: Obeyman.number().optional(),
+    ts: Obeyman.string(),
+});
+
+const target = {
+    userid: 123,
+    pageNum: 2,
+    ts: +new Date()
+};
+
+// validate
+const errStack = Obeyman.validate(target, schema);
+console.log(errStack);
+// output: 
+//  [
+//      { userId: 'is required' },
+//      { ts: 'this field should be string' }
+//  ]
+
+// or use callback
+Obeyman.validate(data, schema, (error, stack) => {
+    if (error) {
+        console.warn(`Validate failed\n`, stack);
+    }
+});
+```
+
+# API Reference
+> Other types(e.g. `array`,`number`,`string`) inherit from  type `any`
+- `#any`
+    - `any.allow`
+    - `any.required`
+    - `any.optional`
+    - `any.len`
+    - `any.min`
+    - `any.max`
+- `#array`
+    - `array.items`
+- `#boolean`
+- `#number`
+- `#object`
+    - `object.keys`
+- `#string`
+    - `string.alphaNumber`
+
+see [joi API Reference doc(8.0.5)](https://github.com/hapijs/joi/blob/v8.0.5/API.md)
--- test.js
+++ test.js
@@ -1,49 +1,44 @@
 var Obeyman = require('.');
 
-// var schema = Obeyman.object().keys({
-//     num: Obeyman.number().min(100).max(120).required().allow(1,2,3),
-//     str: Obeyman.string().len(5),
-//     eq: Obeyman.string('123'),
-//     obj: Obeyman.object().keys({
-//         tar: Obeyman.number().required(),
-//         obj: Obeyman.object().keys({
-//             11: Obeyman.number().optional()
-//         })
-//     }),
-//     arr: Obeyman.array().len(3).optional(),
-//     items: Obeyman.array().items(Obeyman.string().len(3).required())
-// });
+var schema = Obeyman.object().keys({
+    num: Obeyman.number().min(100).max(120).required().allow(1,2,3),
+    str: Obeyman.string().len(5),
+    eq: Obeyman.string('123'),
+    obj: Obeyman.object().keys({
+        tar: Obeyman.number().required(),
+        obj: Obeyman.object().keys({
+            11: Obeyman.number().optional()
+        })
+    }),
+    arr: Obeyman.array().len(3).optional(),
+    items: Obeyman.array().items(Obeyman.string().len(3).required())
+});
 
-// console.log(Obeyman.validate({
-//     num: 121,
-//     str: '1234a123',
-//     obj: {
-//         tar: 1232,
-//         obj: {}
-//     },
-//     arr: [1, 2, 3]
-// }, schema))
+console.log(Obeyman.validate({
+    num: 121,
+    str: '1234a123',
+    obj: {
+        tar: 1232,
+        obj: {}
+    },
+    arr: [1, 2, 3]
+}, schema))
 
-// Obeyman.validate({
-//     num: 1,
-//     eq: 123,
-//     str: '1234a123',
-//     obj: {
-//         // tar: 1232,
-//         obj: {}
-//     },
-//     arr: [1, 2, 3],
-//     items: ['312', 22, 123123]
-// }, schema, function(err, msg) {
-//     console.log(err);
-// })
+Obeyman.validate({
+    num: 1,
+    eq: 123,
+    str: '1234a123',
+    obj: {
+        // tar: 1232,
+        obj: {}
+    },
+    arr: [1, 2, 3],
+    items: ['312', 22, 123123]
+}, schema, function(err, msg) {
+    console.log(err);
+})
 
-// Obeyman.validate([123, '321'], Obeyman.array().items(Obeyman.string().len(3)), function (err, msg) {
-//     console.log(err)
-//     console.log(msg)
-// })
-
-Obeyman.validate('1231', Obeyman.number(), function (err, msg) {
+Obeyman.validate([123, '321'], Obeyman.array().items(Obeyman.string().len(3)), function (err, msg) {
     console.log(err)
     console.log(msg)
 })
